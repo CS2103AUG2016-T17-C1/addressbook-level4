473879b1978affba232de1c2379cf7cdae4889d2
Ming Liang Tay <mltay94@comp.nus.edu.sg>
2016-10-19 23:44:33 +0800

    Allow undo command to work after the clear command

--- src/main/java/seedu/task/logic/commands/ClearCommand.java
+++ src/main/java/seedu/task/logic/commands/ClearCommand.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import seedu.task.model.TaskManager;
 
 /**
@@ -16,25 +15,27 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Task Manager has been cleared!";
     public static final String MESSAGE_FAILURE = "Task Manager has not been cleared";
+    public static final String MESSAGE_NO_TASKS = "0 tasks listed!";
 
-    public ClearCommand() {}
-
+    public ClearCommand() {
+    }
 
     @Override
     public CommandResult execute() {
         assert model != null;
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Clear all tasks");
-        alert.setHeaderText("Are you sure you want to clear ALL TASKS in the task manager?");
-        alert.setContentText("NOTE: This cannot be undone! ");
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            model.resetData(TaskManager.getEmptyTaskManager());
-            return new CommandResult(MESSAGE_SUCCESS);
-        } else {
-            return new CommandResult(MESSAGE_FAILURE);
+        if (model != null) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Clear all tasks");
+            alert.setHeaderText("Are you sure you want to clear ALL TASKS in the task manager?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                model.resetData(TaskManager.getEmptyTaskManager());
+                return new CommandResult(MESSAGE_SUCCESS);
+            } else {
+                return new CommandResult(MESSAGE_FAILURE);
+            }
         }
+        else return new CommandResult(MESSAGE_NO_TASKS);
 
     }
 }
