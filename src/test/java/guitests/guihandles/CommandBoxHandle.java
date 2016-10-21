8500ce70c75c475887033f325f85c776e4d37f18
Ming Liang Tay <mltay94@comp.nus.edu.sg>
2016-10-21 14:59:09 +0800

    Create new alert class and display an alert when user attempts to clear tasks

--- src/test/java/guitests/guihandles/CommandBoxHandle.java
+++ src/test/java/guitests/guihandles/CommandBoxHandle.java
@@ -28,10 +28,10 @@ public class CommandBoxHandle extends GuiHandle{
     public void runCommand(String command) {
         enterCommand(command);
         pressEnter();
-        guiRobot.sleep(200);
+        guiRobot.sleep(10);
         if (command.equals("clear")) // any commands that has an alert dialog that pops out
             pressEnter();
-        guiRobot.sleep(500); //Give time for the command to take effect
+        guiRobot.sleep(700); //Give time for the command to take effect
     }
 
     public HelpWindowHandle runHelpCommand() {
