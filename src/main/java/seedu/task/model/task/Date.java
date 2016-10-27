77138d694090b455f3ef2654f11f862b7ad75a93
Sin Yu Fan <E0003169@u.nus.edu>
2016-10-27 16:19:05 +0800

    Allow traversal through tabs

--- src/main/java/seedu/task/model/task/Date.java
+++ src/main/java/seedu/task/model/task/Date.java
@@ -4,12 +4,19 @@ import seedu.task.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a Task's phone number in the task manager. Guarantees: immutable;
- * is valid as declared in {@link #isValidDueDate(String)}
+ * is valid as declared in {@link #isValidDate(String)}
  */
 public class Date {
 
-    public static final String MESSAGE_DATE_CONSTRAINTS = "Task's date should be in DDMMYYYY format";
-    public static final String DATE_VALIDATION_REGEX = "((0[1-9]|([1-2][0-9])|3[01])(0[1-9]|1[0-2])([2-9]\\d{3})$){0,1}";
+    // @@author A0139284X
+    public static final String MESSAGE_DATE_CONSTRAINTS = "Invalid date, please provide date in DDMMYYYY format";
+    public static final String INTEGER = "\\d*";
+    public static final String DATE_31DAYS_VALIDATION_REGEX = "((0[1-9]|([1-2][0-9])|3[01])(0[13578]|1[02])([2-9]\\d{3})$){0,1}";
+    public static final String DATE_30DAYS_VALIDATION_REGEX = "((0[1-9]|([1-2][0-9])|30)(0[469]|11)([2-9]\\d{3})$){0,1}";
+    public static final String DATE_FEB_NONLEAPYEAR_VALIDATION_REGEX = "((0[1-9]|(1[0-9])|2[0-8])(0[1-9]|1[0-2])([2-9]\\d{3})$){0,1}";
+    public static final String DATE_FEB_LEAPYEAR_VALIDATION_REGEX = "((0[1-9]|(1[0-9])|2[0-9])(0[1-9]|1[0-2])([2-9]\\d{3})$){0,1}";
+
+    // @@author
 
     private final String date;
 
@@ -20,22 +27,47 @@ public class Date {
      *             if given date string is invalid.
      */
     public Date(String date) throws IllegalValueException {
-        if(date == null)
-        	date = "";
-    	date = date.trim();
-        if (!isValidDueDate(date) ) {
+        if (date == null)
+            date = "";
+        date = date.trim();
+        if (!isValidDate(date)) {
             throw new IllegalValueException(MESSAGE_DATE_CONSTRAINTS);
         }
         this.date = date;
     }
 
+    // @@author A0139284X
     /**
      * Returns true if a given string is a valid task date.
      */
-    public static boolean isValidDueDate(String test) {
-        return test.matches(DATE_VALIDATION_REGEX);
+    public static boolean isValidDate(String test) {
+        if (test.matches(INTEGER)) {
+            return test.matches(DATE_31DAYS_VALIDATION_REGEX)
+                    || test.matches(DATE_30DAYS_VALIDATION_REGEX)
+                    || isValidFebDate(test);
+        } else
+            return false;
     }
 
+    private static boolean isValidFebDate(String test) {
+        if (isLeapYear(test)) {
+            return test.matches(DATE_FEB_LEAPYEAR_VALIDATION_REGEX);
+        } else
+            return test.matches(DATE_FEB_NONLEAPYEAR_VALIDATION_REGEX);
+    }
+
+    /**
+     * 
+     * @param test
+     * @return true if is leap year
+     */
+
+    private static boolean isLeapYear(String test) {
+        int year = Integer.parseInt(test);
+        return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+    }
+
+    // @@author
     @Override
     public String toString() {
         return getDate();
@@ -46,7 +78,7 @@ public class Date {
         return other == this // short circuit if same object
                 || (other instanceof Date // instanceof handles nulls
                         && this.getDate().equals(((Date) other).getDate())); // state
-                                                                        // check
+        // check
     }
 
     @Override
--- src/main/java/seedu/task/ui/MainWindow.java
+++ src/main/java/seedu/task/ui/MainWindow.java
@@ -1,9 +1,16 @@
 package seedu.task.ui;
 
+import com.sun.javafx.scene.control.behavior.TabPaneBehavior;
+import com.sun.javafx.scene.control.skin.BehaviorSkinBase;
+import com.sun.javafx.scene.control.skin.TabPaneSkin;
+
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -16,8 +23,8 @@ import seedu.task.model.UserPrefs;
 import seedu.task.model.task.ReadOnlyTask;
 
 /**
- * The Main Window. Provides the basic application layout containing
- * a menu bar and space where other JavaFX elements can be placed.
+ * The Main Window. Provides the basic application layout containing a menu bar
+ * and space where other JavaFX elements can be placed.
  */
 public class MainWindow extends UiPart {
 
@@ -61,6 +68,8 @@ public class MainWindow extends UiPart {
     @FXML
     private AnchorPane statusbarPlaceholder;
 
+    @FXML
+    private TabPane tabPane;
 
     public MainWindow() {
         super();
@@ -83,16 +92,15 @@ public class MainWindow extends UiPart {
         return mainWindow;
     }
 
-    private void configure(String appTitle, String taskManagerName, Config config, UserPrefs prefs,
-                           Logic logic) {
+    private void configure(String appTitle, String taskManagerName, Config config, UserPrefs prefs, Logic logic) {
 
-        //Set dependencies
+        // Set dependencies
         this.logic = logic;
         this.taskManagerName = taskManagerName;
         this.config = config;
         this.userPrefs = prefs;
 
-        //Configure the UI
+        // Configure the UI
         setTitle(appTitle);
         setIcon(ICON);
         setWindowMinSize();
@@ -103,6 +111,50 @@ public class MainWindow extends UiPart {
         setAccelerators();
     }
 
+    // @@author A0139284X
+    /**
+     * Cycle forward and backward through tabs using CTRL+TAB or CTRL+SHIFT+TAB
+     * 
+     */
+
+    public void changeTabs() {
+        KeyCombination combination = new KeyCodeCombination(KeyCode.TAB, KeyCodeCombination.CONTROL_DOWN,
+                KeyCodeCombination.SHIFT_ANY);
+
+        commandBox.getCommandTextField().setOnKeyPressed(event -> {
+            
+            int size = tabPane.getTabs().size();
+            
+            BehaviorSkinBase skin = (BehaviorSkinBase) tabPane.getSkin();
+            TabPaneBehavior tabPaneBehavior = (TabPaneBehavior) skin.getBehavior();
+
+            int selectedIndex = tabPane.getSelectionModel().getSelectedIndex();
+
+            if (combination.match(event)) {
+
+                if (!event.isShiftDown()) {
+                    if (selectedIndex < size - 1) {
+                        tabPaneBehavior.selectNextTab();
+                    } else {
+                        tabPaneBehavior.selectTab(tabPane.getTabs().get(0));
+                    }
+                } else {
+                    if (selectedIndex > 0) {
+                        tabPaneBehavior.selectPreviousTab();
+                    } else {
+                        tabPaneBehavior.selectTab(tabPane.getTabs().get(size - 1));
+                    }
+                }
+            }
+
+        });
+
+        scene.setOnKeyReleased(event -> {
+                commandBox.requestFocus();
+        });
+    }
+
+    // @@author
     private void setAccelerators() {
         helpMenuItem.setAccelerator(KeyCombination.valueOf("F1"));
     }
@@ -111,8 +163,10 @@ public class MainWindow extends UiPart {
         browserPanel = BrowserPanel.load(browserPlaceholder);
         taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
-        statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getTaskManagerFilePath());
+        statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(),
+                config.getTaskManagerFilePath());
         commandBox = CommandBox.load(primaryStage, getCommandBoxPlaceholder(), resultDisplay, logic);
+        this.changeTabs();
     }
 
     private AnchorPane getCommandBoxPlaceholder() {
@@ -160,8 +214,8 @@ public class MainWindow extends UiPart {
      * Returns the current size and the position of the main Window.
      */
     public GuiSettings getCurrentGuiSetting() {
-        return new GuiSettings(primaryStage.getWidth(), primaryStage.getHeight(),
-                (int) primaryStage.getX(), (int) primaryStage.getY());
+        return new GuiSettings(primaryStage.getWidth(), primaryStage.getHeight(), (int) primaryStage.getX(),
+                (int) primaryStage.getY());
     }
 
     @FXML
@@ -193,4 +247,5 @@ public class MainWindow extends UiPart {
     public void releaseResources() {
         browserPanel.freeResources();
     }
+
 }
