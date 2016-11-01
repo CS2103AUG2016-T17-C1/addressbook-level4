27211d81b8a65e441d5ecfcd73ec0bba568bf149
Ming Liang Tay <mltay94@comp.nus.edu.sg>
2016-11-02 00:15:50 +0800

    Create two different functions of the change directory command. One that sets the new directory of the task manager and transfers the tasks from the old directory to the new one, and one that only sets the new directory of the task manager.

--- src/main/java/seedu/task/logic/commands/ChangeDirectoryCommand.java
+++ src/main/java/seedu/task/logic/commands/ChangeDirectoryCommand.java
@@ -13,6 +13,7 @@ import seedu.task.commons.core.LogsCenter;
 import seedu.task.commons.util.ConfigUtil;
 import seedu.task.commons.util.StringUtil;
 import seedu.task.alerts.ChangeDirectoryCommandAlert;
+import seedu.task.alerts.TransferTaskAlert;
 
 //@@author A0142360U
 public class ChangeDirectoryCommand extends Command {
@@ -20,11 +21,12 @@ public class ChangeDirectoryCommand extends Command {
     public static final String COMMAND_WORD = "cd";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Changes the location where the tasks in the Task Manager are saved. "
-            + "Paramaters: cd {file location}/" + "Example: " + COMMAND_WORD + "  cd data/";
+            + "Paramaters: cd {file location}/" + "Example: " + COMMAND_WORD + " data/";
 
     public static final String MESSAGE_SUCCESS = "Directory Changed";
     public static final String MESSAGE_FAILURE = "Illegal directory name given, please use a different directory name and try again";
     public static final String MESSAGE_ERROR = "Error in creating new directory, please try again with another directory";
+    public static final String MESSAGE_CANCELLED = "Change directory action is cancelled";
     private final String newDirectory;
     private Config config;
     private Config newConfig = new Config();
@@ -32,6 +34,11 @@ public class ChangeDirectoryCommand extends Command {
     private static final String DEFAULT_TASK_MANAGER_XML_FILE_NAME = "taskmanager.xml";
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
     private boolean xmlDataTransferSuccess = false;
+    public static final String yes = "Yes";
+    public static final String no = "No";
+    public static final String noAction = "noAction";
+    private static CharSequence[] illegalChars = { "?", "<", ">", ":", "\\", "*", "\0", "|", "=", ";", "[", "]", "{",
+            "}" };
 
     /**
      * Convenience constructor using raw values.
@@ -40,38 +47,24 @@ public class ChangeDirectoryCommand extends Command {
         newDirectory = newDirectory.trim();
         this.config = config;
         this.newDirectory = newDirectory;
-        System.out.println(config+"config");
+        System.out.println(config + "config");
     }
 
     /*
-     * Attempt to change the directory of the taskmanager.xml
-     * and display an Alert that the application has to be restarted if directory is successfully changed
+     * Display an alert that allows users to choose whether they would like to
+     * transfer their current task list to the new specified directory
      */
     @Override
     public CommandResult execute() {
-
-
-        if (isValidDirectory(newDirectory)) {
-            xmlDataTransferSuccess = transferXmlDataToNewFile(newDirectory);
+        String result = TransferTaskAlert.transferTaskOptionAlert();
+        if (result.equals(yes)) {
+            return changeDirectoryAndTransferXmlData(newDirectory);
+        } else if (result.equals(no)) {
+            return changeDirectoryOnly(newDirectory);
         } else {
-            return new CommandResult(MESSAGE_FAILURE);
+            return new CommandResult(MESSAGE_CANCELLED);
         }
 
-        if (xmlDataTransferSuccess) {
-            newConfig.setTaskManagerFilePath(directoryAddXmlExtension(newDirectory));
-            configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
-            try {
-                ConfigUtil.saveConfig(newConfig, configFilePathUsed);
-                displayRestartAlert();
-                return new CommandResult(MESSAGE_SUCCESS);
-
-            } catch (IOException e) {
-                logger.warning("Failed to save new config file : " + StringUtil.getDetails(e));
-                return new CommandResult(MESSAGE_FAILURE);
-            }
-        } else {
-            return new CommandResult(MESSAGE_FAILURE);
-        }
     }
 
     /*
@@ -79,8 +72,9 @@ public class ChangeDirectoryCommand extends Command {
      * end of the directory does not contain the character "/"
      */
     public static boolean isValidDirectory(String directory) {
-        if (directory.contains("?")) {
-            return false;
+        for (CharSequence illegalChar : illegalChars) {
+            if (directory.contains(illegalChar))
+                return false;
         }
         if (!String.valueOf(directory.charAt(directory.length() - 1)).equals("/")) {
             return false;
@@ -107,9 +101,34 @@ public class ChangeDirectoryCommand extends Command {
 
     }
 
+    public CommandResult changeDirectoryAndTransferXmlData(String newDirectory) {
+        if (isValidDirectory(newDirectory)) {
+            xmlDataTransferSuccess = transferXmlDataToNewFile(newDirectory);
+        } else {
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+
+        if (xmlDataTransferSuccess) {
+            newConfig.setTaskManagerFilePath(directoryAddXmlExtension(newDirectory));
+            configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+            try {
+                ConfigUtil.saveConfig(newConfig, configFilePathUsed);
+                displayRestartAlert();
+                return new CommandResult(MESSAGE_SUCCESS);
+
+            } catch (IOException e) {
+                logger.warning("Failed to save new config file : " + StringUtil.getDetails(e));
+                return new CommandResult(MESSAGE_FAILURE);
+            }
+        } else {
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+
+    }
+
     /*
-     * Copy all the tasks from the current taskmanager.xml and pastes them in the new taskmanager.xml file
-     * created in the new directory
+     * Copy all the tasks from the current taskmanager.xml and pastes them in
+     * the new taskmanager.xml file created in the new directory
      */
     public boolean transferXmlDataToNewFile(String newDirectory) {
         try {
@@ -130,6 +149,25 @@ public class ChangeDirectoryCommand extends Command {
         } catch (IllegalStateException | IOException e) {
             logger.warning("The file could not be found");
             return false;
+        }
+
+    }
+
+    /*
+     * Changes the storage location of taskmanager.xml to a new location without
+     * transferring existing tasks over
+     */
+    public CommandResult changeDirectoryOnly(String newDirectory) {
+        newConfig.setTaskManagerFilePath(directoryAddXmlExtension(newDirectory));
+        configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+        try {
+            ConfigUtil.saveConfig(newConfig, configFilePathUsed);
+            displayRestartAlert();
+            return new CommandResult(MESSAGE_SUCCESS);
+
+        } catch (IOException e) {
+            logger.warning("Failed to save new config file : " + StringUtil.getDetails(e));
+            return new CommandResult(MESSAGE_FAILURE);
         }
 
     }
