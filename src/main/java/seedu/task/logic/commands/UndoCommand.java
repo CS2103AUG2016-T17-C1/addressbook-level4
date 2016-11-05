93848a2a6fe6e688df37e2c1393a25417d554345
Ming Liang Tay <mltay94@comp.nus.edu.sg>
2016-11-06 01:34:07 +0800

    Changed message of Undo command

--- src/main/java/seedu/task/logic/commands/UndoCommand.java
+++ src/main/java/seedu/task/logic/commands/UndoCommand.java
@@ -9,8 +9,8 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
     public static final String SHORTCUT = "u";
-    public static final String MESSAGE_SUCCESS = "Your changes are undone";
-    public static final String MESSAGE_FAIL = "No more changes can be undone";
+    public static final String MESSAGE_SUCCESS = "Your changes are undone.";
+    public static final String MESSAGE_FAIL = "No more changes can be undone.";
 
 
     public UndoCommand() {}
